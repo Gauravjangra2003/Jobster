b8208f99b5c98fbb88d3634d53db2edbb8235c4c
Gaurav Jangid <jangidgaurav603@gmail.com>
2025-12-09 21:16:37 +0530

    styled landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,14 +1,16 @@
 import Logo from '../assets/images/logo.svg'
 import Main from '../assets/images/main.svg'
+import styled from 'styled-components'
+
 
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
         <nav>
             <img src={Logo} alt="logoo not found" />
         </nav>
-        <div className="conatiner page">
+        <div className="container page">
             <div className="info">
                 <h1>job <span>tracking</span> app</h1>
                 <p>
@@ -18,8 +20,47 @@ const Landing = () => {
             </div>
             <img src={Main} alt="image not found" className='img main-img' />
         </div>
-    </main>
+    </Wrapper>
   )
 }
+
+
+const Wrapper = styled.main`
+nav{
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+}
+.page{
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+}
+h1{
+    font-weight: 700;
+    span{
+        color: var(--primary-500);
+    }
+}
+p{
+    color: var(--grey-600);
+}
+.main-img{
+    display: none;
+}
+@media (min-width: 992px) {
+    .page{
+        grid-template-columns: 1fr 1fr;
+        column-gap: 3rem;
+    }
+    .main-img{
+        display: block;
+    }
+}
+`
 
 export default Landing
