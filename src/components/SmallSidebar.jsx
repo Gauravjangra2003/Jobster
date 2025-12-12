8574ab3c3e7db046b101f67908eaf7cca1588e4f
Gaurav Jangid <jangidgaurav603@gmail.com>
2025-12-12 13:15:43 +0530

    feat : small sidebar and big sidebar

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,8 +1,35 @@
-import Wrapper from "../assets/wrappers/SmallSidebar";
-const SmallSidebar = () => {
-  return (
-    <Wrapper><h2>SmallSidebar</h2></Wrapper>
-  )
-}
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import Logo from './Logo';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleSidebar } from '../features/user/userSlice';
+import NavLinks from './Navlinks';
 
-export default SmallSidebar
+const SmallSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button className='close-btn' onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggle} />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+export default SmallSidebar;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,6 +3,7 @@ import FormRow from './FormRow'
 import Navbar from './Navbar'
 import SmallSidebar from './SmallSidebar'
 import BigSidebar from './BigSidebar'
+import NavLinks from './NavLinks'
 
 
-export { Logo , FormRow , Navbar, SmallSidebar, BigSidebar}
+export { Logo , FormRow , Navbar, SmallSidebar, BigSidebar , NavLinks }
