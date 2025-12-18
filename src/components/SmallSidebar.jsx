d3bbc903c2c4408ac702ffd8d8f67ca321ed38bc
Gaurav Jangid <jangidgaurav603@gmail.com>
2025-12-18 16:24:48 +0530

    fix :  import error 2

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa';
 import Logo from './Logo';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleSidebar } from '../features/user/userSlice';
-import NavLinks from './NavLinks';
+import NavLinks from './Navlinks';
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user);
