43acec6b8c0f899d85c0c39129120b397f37ddfa
Gaurav Jangid <jangidgaurav603@gmail.com>
2025-12-18 14:10:22 +0530

    refactor unauthorised requests

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { getUserFromLocalStorage } from "./localstorage";
+import { clearStore } from "../features/user/UserSlice";
 
 const customFetch = axios.create({
   baseURL: "https://redux-toolkit-jobster-api-server.onrender.com/api/v1",
@@ -20,5 +21,13 @@ customFetch.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+export const checkForUnauthorizedResponse = (error , thunkAPI) => {
+   if(error.response.status === 401){
+            thunkAPI.dispatch(clearStore());
+            return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+        }
+        return thunkAPI.rejectWithValue(error.response.data.msg);
+}
 
 export default customFetch;
